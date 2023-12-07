16d08e618ef226e839de70ac6a8b4b968828e79a
Kağan Küçük <kagan.kucuk@getir.com>
2023-12-07 20:50:11 +0300

    routes and handlers

--- main.go
+++ main.go
@@ -1,30 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	application "github.com/kaankucukx/building-a-microservice-with-go/app"
 )
 
 func main() {
-	router := chi.NewRouter()
+	app := application.New()
+	err := app.Start(context.TODO())
 
-	router.Use(middleware.Logger)
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Println("failed to listen server", err)
+		fmt.Println("failed to start app:", err)
 	}
 }
 
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world"))
-}
+
